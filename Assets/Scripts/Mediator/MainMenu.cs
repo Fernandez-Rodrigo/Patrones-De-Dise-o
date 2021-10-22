996a67a2526a564776458fdf2d7bbe8d78a35e2a
toty <rodrigoo.fernandez@gmail.com>
2021-10-22 12:06:15 -0300

    22/10

---
+++ Assets/Scripts/Mediator/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button _selectPattern;
+    [SerializeField] private Button _settings;
+    [SerializeField] private CanvasGroup _canvasGroup;
+
+    MediatorMenu _mediator;
+
+
+
+}
---
+++ Assets/Scripts/Mediator/MediatorMenu.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediatorMenu : MonoBehaviour
+{
+    [SerializeField] private MainMenu _mainMenu;
+    [SerializeField] private SettingsMenu _settingsMenu;
+}
